8057db94a9dcd724656d85430402048e06e356f6
Jason Stajich <jasonstajich.phd@gmail.com>
2023-11-09 12:59:34 -0800

    Update downloads.sh
    remove mistaken line

--- downloads.sh
+++ downloads.sh
@@ -3,7 +3,7 @@
 mkdir db
 pushd db
 URL=ftp://ftp.ncbi.nlm.nih.gov/refseq/TargetedLoci/Fungi
-fungi.ITS.fna.gz
+
 for db in fungi.ITS.fna.gz fungi.ITS.gbff.gz fungi.18SrRNA.fna.gz fungi.18SrRNA.gbff.gz fungi.28SrRNA.fna.gz fungi.28SrRNA.gbff.gz
 do
     curl -O $URL/$db
